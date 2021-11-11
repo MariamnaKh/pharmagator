cbfdaec5cf7d7e76c7a78a245ac41c486ed64b71
MarianaKh <mariana.khovanets@gmail.com>
2021-11-11 11:56:32 +0200

    add pharmacyName field

--- api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/MedicineDto.java
+++ api/src/main/java/com/eleks/academy/pharmagator/dataproviders/dto/MedicineDto.java
@@ -1,5 +1,6 @@
 package com.eleks.academy.pharmagator.dataproviders.dto;
 
+import com.univocity.parsers.annotations.Parsed;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,10 +10,16 @@ import java.math.BigDecimal;
 @Builder
 public class MedicineDto {
 
+    @Parsed(field = "medicine")
     private String title;
 
+    @Parsed
     private BigDecimal price;
 
+    @Parsed(field = "link")
     private String externalId;
+
+    @Parsed(field = "pharmacy")
+    private String pharmacyName;
 
 }
