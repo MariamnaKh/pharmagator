146335a1a0984ebd1d984551b2671a87ec04a5d8
MarianaKh <mariana.khovanets@gmail.com>
2021-11-15 17:42:23 +0200

    add response entity ok

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/CsvController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/CsvController.java
@@ -4,20 +4,23 @@ import com.eleks.academy.pharmagator.services.impl.CsvUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("medicines/csv")
 public class CsvController {
 
     private final CsvUploadService csvUploadService;
 
-    @PostMapping
-    public ResponseEntity<?> upload(@RequestParam MultipartFile multipartFile) {
+    @PostMapping("/upload")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile multipartFile) {
         if (!multipartFile.isEmpty() && multipartFile.getContentType().contentEquals("text/csv")) {
             csvUploadService.save(multipartFile);
+            return ResponseEntity.ok().build();
         }
         return ResponseEntity.noContent().build();
     }
