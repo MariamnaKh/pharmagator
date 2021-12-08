a342676d834b9736b174407da7f2df9b71a3aa9a
MarianaKh <mariana.khovanets@gmail.com>
2021-12-08 13:18:54 +0200

    add all args constructor

--- api/src/main/java/com/eleks/academy/pharmagator/exceptions/handler/Response.java
+++ api/src/main/java/com/eleks/academy/pharmagator/exceptions/handler/Response.java
@@ -1,5 +1,6 @@
 package com.eleks.academy.pharmagator.exceptions.handler;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Response {
 
     private String message;
