ba5fc1d5f16e0e4e35db4912bd0bee80d7b01201
MarianaKh <mariana.khovanets@gmail.com>
2021-12-08 20:37:40 +0200

    add builder annotation

--- api/src/main/java/com/eleks/academy/pharmagator/entities/Pharmacy.java
+++ api/src/main/java/com/eleks/academy/pharmagator/entities/Pharmacy.java
@@ -1,6 +1,7 @@
 package com.eleks.academy.pharmagator.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import javax.persistence.*;
 
 @Data
 @Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "pharmacies")
