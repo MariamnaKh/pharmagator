4fe6d5f4ff6ffb67a4274777f9860a3005278a9d
MarianaKh <84643950+MariamnaKh@users.noreply.github.com>
2021-12-10 23:50:58 +0200

    data export (#7)
    * export to pdf
    
    * no args & all args constructors
    
    * fix medicine dto

--- api/src/main/java/com/eleks/academy/pharmagator/repositories/PriceRepository.java
+++ api/src/main/java/com/eleks/academy/pharmagator/repositories/PriceRepository.java
@@ -2,14 +2,24 @@ package com.eleks.academy.pharmagator.repositories;
 
 import com.eleks.academy.pharmagator.entities.Price;
 import com.eleks.academy.pharmagator.entities.PriceId;
+import com.eleks.academy.pharmagator.projections.MedicinePrice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PriceRepository extends JpaRepository<Price, PriceId> {
 
     Optional<Price> findByPharmacyIdAndMedicineId(Long pharmacyId, Long priceId);
+
+    @Query("""
+            SELECT p.price as price, m.title as title, p.pharmacyId as pharmacyId
+            FROM Price p
+            LEFT JOIN Medicine m ON m.id = p.medicineId
+            """)
+    List<MedicinePrice> findAllMedicinesPrices();
 
 }
